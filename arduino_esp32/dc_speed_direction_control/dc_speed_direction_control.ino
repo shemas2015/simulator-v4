cc2fabe765f25ee7eb88ff41267c391d24a61696
Juan Sebastian Daza <juan.d@montechelo.com.co>
2025-08-26 12:48:14 -0500

    Input by serial moving the motor actuator according the serial input angle and speed when starts, positioning the actuator on 90 grade +-5

--- arduino_esp32/dc_speed_direction_control/dc_speed_direction_control.ino
+++ arduino_esp32/dc_speed_direction_control/dc_speed_direction_control.ino
@@ -14,6 +14,13 @@
 #define R_EN_PIN 27  // Right Enable
 #define L_EN_PIN 14  // Left Enable
 
+
+// Global variables for serial input
+int inputSpeed = 0;         // No default speed
+float targetAngle = 0.0;    // No default target angle
+bool parametersSet = false; // Flag to track if parameters have been set
+
+
 void setup() {
   Serial.begin(9600);
   Wire.begin();
@@ -33,8 +40,41 @@ void setup() {
   // Enable IBT-2
   digitalWrite(R_EN_PIN, HIGH);
   digitalWrite(L_EN_PIN, HIGH);
+  
+  // Initialize motor in stopped state
+  analogWrite(RPWM_PIN, 0);
+  analogWrite(LPWM_PIN, 0);
 
   Serial.println("MPU6050 and IBT-2 initialized");
+  Serial.println("Moving to initial position (85-95 degrees)...");
+  
+  // Initial positioning - move motor to between 85-95 degrees
+  delay(1000); // Wait for MPU6050 to stabilize
+  
+  while (true) {
+    // Read accelerometer data
+    int16_t accelX = readMPU6050(ACCEL_XOUT_H);
+    int16_t accelY = readMPU6050(ACCEL_XOUT_H + 2);
+    int16_t accelZ = readMPU6050(ACCEL_XOUT_H + 4);
+
+    // Calculate current angle
+    float currentAngle = atan2(-accelX, sqrt(accelY * accelY + accelZ * accelZ)) * 180.0 / PI;
+    currentAngle += 90;
+    
+    // Position motor to 90 degrees (center of 85-95 range)
+    positionMotor(currentAngle, 90.0, 80); // Use moderate speed for initial positioning
+    
+    // Check if we're in the target range
+    if (currentAngle >= 85.0 && currentAngle <= 95.0) {
+      motorStop(currentAngle);
+      Serial.println("Initial position reached!");
+      break;
+    }
+    
+    delay(100);
+  }
+  
+  Serial.println("Motor stopped - waiting for input (format: speed,angle)");
 }
 
 // Function to print angle and motor status
@@ -65,8 +105,46 @@ void motorStop(float angle) {
   delay(500);
 }
 
+// Function to position motor based on current and target angles
+void positionMotor(float currentAngle, float targetAngle, int speed) {
+  float angleDifference = currentAngle - targetAngle;
+  
+  if (abs(angleDifference) <= 5.0) {
+    // Within ±5 degrees of target - stop motor
+    motorStop(currentAngle);
+    parametersSet = false;
+  }
+  else if (currentAngle > targetAngle) {
+    // Current angle is greater than target - move forward
+    motorForward(speed, currentAngle);
+  } 
+  else {
+    // Current angle is less than target - move backward
+    motorBackward(speed, currentAngle);
+  }
+}
+
+
+
 void loop() {
-  int spd = 60;
+  // Check for serial input
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    input.trim();
+    
+    // Parse input format: "speed,angle" (e.g., "100,45")
+    int commaIndex = input.indexOf(',');
+    if (commaIndex > 0) {
+      inputSpeed = input.substring(0, commaIndex).toInt();
+      targetAngle = input.substring(commaIndex + 1).toFloat();
+      parametersSet = true; // Mark that parameters have been set
+      
+      Serial.print("New settings - Speed: ");
+      Serial.print(inputSpeed);
+      Serial.print(", Target Angle: ");
+      Serial.println(targetAngle);
+    }
+  }
   
   // Read accelerometer data
   int16_t accelX = readMPU6050(ACCEL_XOUT_H);
@@ -74,24 +152,22 @@ void loop() {
   int16_t accelZ = readMPU6050(ACCEL_XOUT_H + 4);
 
   // Calculate tilt angle in degrees (horizontal axis)
-  float angleY = atan2(-accelX, sqrt(accelY * accelY + accelZ * accelZ)) * 180.0 / PI;
+  float currentAngle = atan2(-accelX, sqrt(accelY * accelY + accelZ * accelZ)) * 180.0 / PI;
   
   // Add 90 degrees to make 0 degrees horizontal
-  angleY += 90;
+  currentAngle += 90;
 
-  // Motor control based on angle
-  if (angleY > 90) {
-    motorForward(spd, angleY);
-  } 
-  else if (angleY < 85) {
-    motorBackward(spd, angleY);
-  } 
-  else {
-    // Angle between 85-90 degrees
-    motorStop(angleY);
+  // Only move motor if parameters have been set via serial input
+  if (parametersSet) {
+    positionMotor(currentAngle, targetAngle, inputSpeed);
+  } else {
+    // Keep motor stopped until parameters are set
+    analogWrite(RPWM_PIN, 0);
+    analogWrite(LPWM_PIN, 0);
   }
 
   delay(100);
+  
 }
 
 int16_t readMPU6050(uint8_t reg) {
